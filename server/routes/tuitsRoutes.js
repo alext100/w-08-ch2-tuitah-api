8ee41343ad6746d8d92bb1fda576b24e317c81c6
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-17 21:41:49 +0100

    Router post with '/createtuit' added

--- server/routes/tuitsRoutes.js
+++ server/routes/tuitsRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
-const { getTuits } = require("../controllers/tuitsControllers");
+const { getTuits, createTuit } = require("../controllers/tuitsControllers");
 
 const router = express.Router();
 
 router.get("/", getTuits);
+
+router.post("/createtuit", createTuit);
 
 module.exports = router;
