7cb883e2ebb5198e66dbf2644858adeda2aaf2b5
SandraQS <sanquesan@gmail.com>
2021-11-17 21:46:26 +0100

    Change route at index.js

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/", tuitsRoutes);
+app.use("/tuits", tuitsRoutes);
 
 app.use(handlerNotFound);
 app.use(handlerGeneralError);
