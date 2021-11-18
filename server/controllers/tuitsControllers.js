ae7af4eb1ff37a252b7b2cf579794be7533f209e
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-18 19:28:05 +0100

    Function deleteTuit refactored, .status(200) added

--- server/controllers/tuitsControllers.js
+++ server/controllers/tuitsControllers.js
@@ -2,7 +2,7 @@ const Tuit = require("../../database/models/Tuit");
 
 const getTuits = async (req, res, next) => {
   try {
-    const tuits = await Tuit.find().sort({ date: 1 });
+    const tuits = await Tuit.find();
     res.json(tuits);
   } catch {
     const error = new Error("No encontrado");
@@ -27,10 +27,16 @@ const deleteTuit = async (req, res, next) => {
   const { id } = req.params;
   try {
     const tuit = await Tuit.findByIdAndRemove(id);
-    res.json(tuit);
+    if (!tuit) {
+      const error = new Error("Tuit not found");
+      error.code = 404;
+      return next(error);
+    } else {
+      res.status(200).json(tuit);
+    }
   } catch (error) {
     error.code = 400;
-    error.message = "Tuit not found";
+    error.message = "Error on delete tuit";
     next(error);
   }
 };
