318042bdddb03b5655643e3696857d513990fd32
SandraQS <sanquesan@gmail.com>
2021-11-18 20:32:00 +0100

    Resolve conflicts at routes

--- server/routes/tuitsRoutes.js
+++ server/routes/tuitsRoutes.js
@@ -1,18 +1,13 @@
 const express = require("express");
 /* const { validate } = require("express-validation"); */
+// const { tuitValidation } = require("../schemas/tuitSchema");
 
 const {
   getTuits,
   createTuit,
-<<<<<<< HEAD
   addFriend,
-} = require("../controllers/tuitsControllers");
-const { tuitValidation } = require("../schemas/tuitSchema");
-=======
   deleteTuit,
 } = require("../controllers/tuitsControllers");
-/* const { tuitValidation } = require("../schemas/tuitSchema"); */
->>>>>>> master
 
 const router = express.Router();
 
@@ -20,10 +15,8 @@ router.get("/", getTuits);
 
 router.post("/createtuit", /*  validate(tuitValidation), */ createTuit);
 
-<<<<<<< HEAD
 router.patch("/patch", addFriend);
-=======
+
 router.delete("/delete/:id", deleteTuit);
->>>>>>> master
 
 module.exports = router;
